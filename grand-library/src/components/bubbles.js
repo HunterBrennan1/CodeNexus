df456ddf8227d475d68ca12b049e68556b2c4798
Hunter <hunterbrennan15@gmail.com>
2023-07-04 13:34:11 -0400

    added content to bubbles section

--- grand-library/src/components/bubbles.js
+++ grand-library/src/components/bubbles.js
@@ -1,3 +1,7 @@
+import Penicon from '../assets/imgs/penicon.svg'
+import Database from '../assets/imgs/sqlicon.svg'
+import Computericon from '../assets/imgs/computericon.svg'
+import Layers from '../assets/imgs/layerstackicon.svg'
 
 
 export const Bubbles = () => {
@@ -5,9 +9,24 @@ export const Bubbles = () => {
     <section className="bubbles_section_wrapper">
       <div className="bubbles_container">
         <div className="bubble">
-          <img className="bubble_img"></img>
-          <h2 className="bubble_title"></h2>
-          <p className="bubble_text"></p>
+          <img className="bubble_img" src={Penicon}></img>
+          <h2 className="bubble_title">Styling Tools</h2>
+          <p className="bubble_text">Find useful tools for creative inspiration</p>
+        </div>
+        <div className="bubble">
+          <img className="bubble_img" src={Database}></img>
+          <h2 className="bubble_title">Styling Tools</h2>
+          <p className="bubble_text">Find useful tools for creative inspiration</p>
+        </div>
+        <div className="bubble">
+          <img className="bubble_img" src={Computericon}></img>
+          <h2 className="bubble_title">Styling Tools</h2>
+          <p className="bubble_text">Find useful tools for creative inspiration</p>
+        </div>
+        <div className="bubble">
+          <img className="bubble_img" src={Layers}></img>
+          <h2 className="bubble_title">Styling Tools</h2>
+          <p className="bubble_text">Find useful tools for creative inspiration</p>
         </div>
       </div>
     </section>
