2a087a9008a2e4dbe9ba7ad7a7b5cb50c17c5ce4
Hunter <hunterbrennan15@gmail.com>
2023-07-04 14:19:56 -0400

    completed styling and content on bubbles and completed mobile responsiveness

--- grand-library/src/components/bubbles.js
+++ grand-library/src/components/bubbles.js
@@ -2,6 +2,7 @@ import Penicon from '../assets/imgs/penicon.svg'
 import Database from '../assets/imgs/sqlicon.svg'
 import Computericon from '../assets/imgs/computericon.svg'
 import Layers from '../assets/imgs/layerstackicon.svg'
+import Magnifier from '../assets/imgs/magnifiericon.svg'
 
 
 export const Bubbles = () => {
@@ -18,29 +19,29 @@ export const Bubbles = () => {
         <div className="bubble">
           <div className='bubble_inside'>
             <img className="bubble_img" src={Database}></img>
-            <h2 className="bubble_title">Styling Tools</h2>
-            <p className="bubble_text">Find useful tools for creative inspiration</p>
+            <h2 className="bubble_title">Save Tools</h2>
+            <p className="bubble_text">Login and save apps to your homepage</p>
           </div>
         </div>
         <div className="bubble">
           <div className='bubble_inside'>
             <img className="bubble_img" src={Computericon}></img>
-            <h2 className="bubble_title">Styling Tools</h2>
-            <p className="bubble_text">Find useful tools for creative inspiration</p>
+            <h2 className="bubble_title">Design Smart</h2>
+            <p className="bubble_text">Work smarter not harder!</p>
           </div>
         </div>
         <div className="bubble">
           <div className='bubble_inside'>
             <img className="bubble_img" src={Layers}></img>
-            <h2 className="bubble_title">Styling Tools</h2>
-            <p className="bubble_text">Find useful tools for creative inspiration</p>
+            <h2 className="bubble_title">For Developers</h2>
+            <p className="bubble_text">Made by developers, For developers</p>
           </div>
         </div>
         <div className="bubble">
           <div className='bubble_inside'>
-            <img className="bubble_img" src={Layers}></img>
-            <h2 className="bubble_title">Styling Tools</h2>
-            <p className="bubble_text">Find useful tools for creative inspiration</p>
+            <img className="bubble_img" src={Magnifier}></img>
+            <h2 className="bubble_title">Look Around!</h2>
+            <p className="bubble_text">Searching made easier for you</p>
           </div>
         </div>
       </div>
