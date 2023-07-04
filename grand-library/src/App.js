9ac3c28b9caf8861a78d5db9dd6b96e41139ffd8
Hunter <hunterbrennan15@gmail.com>
2023-07-04 00:54:28 -0400

    added hero banner text

--- grand-library/src/App.js
+++ grand-library/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbartop from './components/navbar';
+import Hero from './components/hero';
 
 
 function App() {
   return (
     <div className="App">
       <Navbartop />
+      <Hero />
     </div>
   );
 }
---
+++ grand-library/src/components/hero.js
@@ -0,0 +1,18 @@
+
+
+export const Hero = () => {
+  return (
+    <section className="homepage_wrapper">
+      <div className="homepage_hero_container">
+        <div className="homepage_hero_text_container">
+          <h1 className="homepage_hero_title">Developing made easier for you!</h1>
+          <p className="homepage_hero_text">find the perfect library or tool or get inspired by other developers.</p>
+          <a className="explore_btn btn--primary">Start Exploring!</a>
+          <a className="about_us_btn">Learn about us</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
