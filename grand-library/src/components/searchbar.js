33e7d68a233b7d16f37a2fe93473d00b8c30b9d1
Hunter <hunterbrennan15@gmail.com>
2023-07-18 22:35:05 -0400

    added images and title to match the search options

--- grand-library/src/components/searchbar.js
+++ grand-library/src/components/searchbar.js
@@ -4,6 +4,20 @@ import { items } from './toolslist';
 import Gradienthunt from '../assets/imgs/gradienthunt.PNG';
 import Cardhero from './cardpagehero';
 import Refresh from '../assets/imgs/refresh.svg';
+import Boxshadow from "../assets/imgs/boxshadow.PNG";
+import Haikei from "../assets/imgs/haikei.PNG";
+import Bootstrap from "../assets/imgs/bootstrap.PNG";
+import Keyframes from "../assets/imgs/keyframes.PNG";
+import Fullpage from "../assets/imgs/fullpage.PNG";
+import Anime from "../assets/imgs/animejs.PNG";
+import Swiper from "../assets/imgs/swiperjs.PNG";
+import Threejs from "../assets/imgs/threejs.PNG";
+import Leaflet from "../assets/imgs/leaflet.PNG";
+import Mui from "../assets/imgs/MUI.PNG";
+import Reactbootstrap from "../assets/imgs/reactbootstrap.PNG";
+import Nivo from "../assets/imgs/nivo.PNG";
+import Undraw from "../assets/imgs/undraw.PNG";
+import Framer from "../assets/imgs/framer.PNG";
 
 export const Searchbar = () => {
   const [selectedFilters, setSelectedFilters] = useState([]);
@@ -37,7 +51,24 @@ export const Searchbar = () => {
 
   const [searchText, setSearchText] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-  const options = ['Gradient Hunt', 'Box-Shadow', 'Haikei', 'Bootstrap', 'KeyFrames', 'FullPage.js', 'Anime.js', 'Swiper.js', 'Three.js', 'Leaflet.js', 'MUI', 'React-Bootstrap', 'Nivo', 'Undraw.Co', 'Framer Motion'];
+  const options = [
+    { label: 'Gradient Hunt', image: Gradienthunt, link: 'https://gradienthunt.com/' },
+    { label: 'Box-Shadow', image: Boxshadow, link: 'https://gradienthunt.com/' },
+    { label: 'Haikei', image: Haikei, link: 'https://gradienthunt.com/' },
+    { label: 'Bootstrap', image: Bootstrap, link: 'https://gradienthunt.com/' },
+    { label: 'KeyFrames', image: Keyframes, link: 'https://gradienthunt.com/' },
+    { label: 'FullPage.js', image: Fullpage, link: 'https://gradienthunt.com/' },
+    { label: 'Anime.js', image: Anime, link: 'https://gradienthunt.com/' },
+    { label: 'Swiper.js', image: Swiper, link: 'https://gradienthunt.com/' },
+    { label: 'Three.js', image: Threejs, link: 'https://gradienthunt.com/' },
+    { label: 'Leaflet.js', image: Leaflet, link: 'https://gradienthunt.com/' },
+    { label: 'MUI', image: Mui, link: 'https://gradienthunt.com/' },
+    { label: 'React-bootstrap', image: Reactbootstrap, link: 'https://gradienthunt.com/' },
+    { label: 'Nivo', image: Nivo, link: 'https://gradienthunt.com/' },
+    { label: 'Undraw.co', image: Undraw, link: 'https://gradienthunt.com/' },
+    { label: 'Framer Motion', image: Framer, link: 'https://gradienthunt.com/' },
+    // ...
+  ];
 
   const handleInputChange = (event) => {
     const value = event.target.value;
@@ -45,13 +76,13 @@ export const Searchbar = () => {
 
     // Filter options based on input value
     const filteredOptions = options.filter((option) =>
-      option.toLowerCase().includes(value.toLowerCase())
+      option.label.toLowerCase().includes(value.toLowerCase())
     );
     setSearchResults(filteredOptions);
   };
 
   const handleOptionClick = (option) => {
-    setSearchText(option);
+    setSearchText(option.label);
     setSearchResults([]);
   };
 
@@ -68,15 +99,22 @@ export const Searchbar = () => {
           <input type="text" value={searchText} onChange={handleInputChange} className='search_input' placeholder='Search or Filter by languages, styles, and more...'></input>
           <div>
             <button className="clear-button" onClick={handleClearInput}>
-              {/* <img className='clear-button_img' src={Refresh}></img> */}
+
             </button>
           </div>
         </div>
         {searchResults.length > 0 && (
           <ul className="autocomplete-options">
             {searchResults.map((option) => (
-              <li key={option} onClick={() => handleOptionClick(option)}>
-                {option}
+              <li className='search_option' key={option.label} onClick={() => handleOptionClick(option)}>
+                {option.image && (
+                  <img
+                    className="option-image"
+                    src={option.image}
+                    alt=""
+                  />
+                )}
+                {option.label}
 
               </li>
             ))}
